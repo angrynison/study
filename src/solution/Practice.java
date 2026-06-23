e26d82a6a9c615c779dcf939ebe7bfd117b1559e
angrynison <kalina01255@gmail.com>
2026-06-23 17:08:49 +0900

    알고리즘 공부

--- src/solution/Practice.java
+++ src/solution/Practice.java
@@ -6,48 +6,46 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
-//가장큰수 - 정렬
+// 코딩복습파일
 public class Practice {
     public static void main(String[] args) throws IOException{
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int target =  Integer.parseInt(st.nextToken());
         st =  new StringTokenizer(br.readLine());
-        int []arr= new int[n];
-        for (int i = 0; i < n; i++) {
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
-        int start_node = 0;
-        int end_node = n-1;
         int count = 0;
 
-        while(start_node < end_node){
+        for (int index = 2; index < n; index++){
+            int start_node = 0;
+            int end_node = index-1;
+            while (start_node < end_node){
+                if (arr[start_node] + arr[end_node] == arr[index]){
+                    count++;
+                    start_node++;
+                    end_node--;
+                    break;
+                }
 
-            if(arr[start_node] + arr[end_node] < target){
-                start_node++;
-                continue;
+                if (arr[start_node] + arr[end_node] < arr[index]){
+                    start_node++;
+                    continue;
+                }
+
+                if (arr[start_node] + arr[end_node] > arr[index]){
+                    end_node--;
+                    continue;
+                }
             }
 
-            if(arr[start_node] + arr[end_node] > target){
-                end_node--;
-                continue;
-            }
-
-            if(arr[start_node] + arr[end_node] == target){
-                count++;
-                start_node++;
-                end_node--;
-                continue;
-            }
         }
-
         System.out.println(count);
     }
 }
