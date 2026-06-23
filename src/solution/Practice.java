77a0fe27f58798da0550c98a7056c825c81eb869
angrynison <kalina01255@gmail.com>
2026-06-23 16:33:16 +0900

    알고리즘

--- src/solution/Practice.java
+++ src/solution/Practice.java
@@ -12,41 +12,41 @@ public class Practice {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int q = Integer.parseInt(st.nextToken());
 
-        int [][] arr = new int[n+1][n+1];
-        int [][] board = new int[n+1][n+1];
-        int [][] ques = new int[q][4];
+        int start_node = 1;
+        int end_node = 1;
+        int count = 1;
+        int[] arr = new int[n+1];
 
-        for(int i = 1; i<=n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 1; j<=n; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= n; i++){
+            arr[i] = i;
+        }
+
+        int sum = arr[1];
+
+        while(end_node < n){
+
+            if(sum < n){
+                end_node++;
+                sum += arr[end_node];
+                continue;
+            }
+
+            if(sum > n) {
+                sum -= arr[start_node];
+                start_node++;
+                continue;
+            }
+
+            if(sum == n){
+                count++;
+                end_node++;
+                sum += arr[end_node];
+                continue;
             }
         }
 
-        for(int i = 0; i<q; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<4; j++){
-                ques[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=n; j++){
-                board[i][j] = arr[i][j] + board[i][j-1] + board[i-1][j] - board[i-1][j-1];
-            }
-        }
-
-
-        for (int i = 0; i < q; i++) {
-            int x1 = ques[i][0];
-            int y1 = ques[i][1];
-            int x2 = ques[i][2];
-            int y2 = ques[i][3];
-
-            System.out.println(board[x2][y2] - board[x2][y1-1] - board[x2][y1-1] + board[x1-1][y1-1]);
-        }
+        System.out.println(count);
 
     }
 }
