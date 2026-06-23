580b79498575b41caaae2b87cf57666efab15fa5
angrynison <kalina01255@gmail.com>
2026-06-23 16:56:12 +0900

    알고리즘

--- src/solution/Practice.java
+++ src/solution/Practice.java
@@ -3,50 +3,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 //가장큰수 - 정렬
 public class Practice {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-
-        int start_node = 1;
-        int end_node = 1;
-        int count = 1;
-        int[] arr = new int[n+1];
-
-        for(int i = 1; i <= n; i++){
-            arr[i] = i;
+        st = new StringTokenizer(br.readLine());
+        int target =  Integer.parseInt(st.nextToken());
+        st =  new StringTokenizer(br.readLine());
+        int []arr= new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int sum = arr[1];
+        Arrays.sort(arr);
 
-        while(end_node < n){
+        int start_node = 0;
+        int end_node = n-1;
+        int count = 0;
 
-            if(sum < n){
-                end_node++;
-                sum += arr[end_node];
-                continue;
-            }
+        while(start_node < end_node){
 
-            if(sum > n) {
-                sum -= arr[start_node];
+            if(arr[start_node] + arr[end_node] < target){
                 start_node++;
                 continue;
             }
 
-            if(sum == n){
+            if(arr[start_node] + arr[end_node] > target){
+                end_node--;
+                continue;
+            }
+
+            if(arr[start_node] + arr[end_node] == target){
                 count++;
-                end_node++;
-                sum += arr[end_node];
+                start_node++;
+                end_node--;
                 continue;
             }
         }
 
         System.out.println(count);
-
     }
 }
